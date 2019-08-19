2b3e0f88537f7dde6d9528e1eb90429aaaccf122
VinceBarry <1004743668@qq.com>
2019-08-19 10:39:12 +0800

    [add] add "zuzhang"

--- src/ZuZhang.java
+++ src/ZuZhang.java
@@ -2,5 +2,6 @@ public class ZuZhang {
     public static void main(String[] args) {
         System.out.println("XiaoYuan");
         System.out.println("NewMethod");
+        System.out.println("ZuZhang");
     }
 }
